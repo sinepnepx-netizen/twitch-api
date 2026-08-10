d70481a83bc0d94f7c4234cdf91a901e4d546d03
sinepnepx-netizen <sinepnepx@gmail.com>
2026-08-10 04:39:26 -0300

    Create comando.js

---
+++ api/comando.js
@@ -0,0 +1,32 @@
+import fs from "fs";
+import path from "path";
+
+export default function handler(req, res) {
+  const { comando, user = "Alguém", target = "Alguém" } = req.query;
+
+  const arquivo = path.join(
+    process.cwd(),
+    "frases",
+    `${comando}.json`
+  );
+
+  if (!fs.existsSync(arquivo)) {
+    return res.status(404).send("Comando não encontrado.");
+  }
+
+  const frases = JSON.parse(
+    fs.readFileSync(arquivo, "utf8")
+  );
+
+  const frase =
+    frases[Math.floor(Math.random() * frases.length)]
+      .replaceAll("{user}", `@${user}`)
+      .replaceAll("{target}", `@${target}`);
+
+  res.setHeader(
+    "Content-Type",
+    "text/plain; charset=utf-8"
+  );
+
+  res.status(200).send(frase);
+}
